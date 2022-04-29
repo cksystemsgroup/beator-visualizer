469ed4a165740500dd375d4f1842d4b0f58d6d8f
geo-bert <s1067297@stud.sbg.ac.at>
2022-04-29 18:55:57 +0200

    Add more local information to widget

--- src/components/LocalInfo.tsx
+++ src/components/LocalInfo.tsx
@@ -9,9 +9,21 @@ function LocalInfo({ target }: { target: ModelNode | undefined }) {
         <p>No node selected!</p>
       ) : (
         <ul>
-          <li>{target.nid}</li>
-          <li>local</li>
-          <li>local</li>
+          <li>
+            <b>Type:</b> {target.type}
+          </li>
+          <li>
+            <b>NID:</b> {target.nid}
+          </li>
+          <li>
+            <b>Depth:</b> {target.stats.depth}
+          </li>
+          <li>
+            <b>Entanglement:</b> {target.stats.entanglement}
+          </li>
+          <li>
+            <b>Collapsed:</b> {target.view.collapsed ? "true" : "false"}
+          </li>
         </ul>
       )}
     </details>
