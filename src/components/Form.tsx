94fdab0de8d24ee24800e2fb704517e7b5cbe048
geo-bert <s1067297@stud.sbg.ac.at>
2022-05-15 11:35:59 +0200

    Style form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -5,23 +5,27 @@ import { SetStringQ } from "../types/react-types";
 function Form({ setText }: { setText: SetStringQ }) {
   let [file, setFile] = useState<File>();
   return (
-    <form>
+    <form className="file-form">
+      Select a valid BEATOR2 Model File <br />
       <input
         type="file"
         accept=".btor2"
         onChange={(e) => setFile(e.currentTarget.files!.item(0)!)}
       />
       <br />
-      <button
+      <input
         type="button"
         disabled={!file}
-        onClick={() => file!.text().then(setText)}>
-        Calculate
-      </button>
+        onClick={() => file!.text().then(setText)}
+        value="Evaluate Model"
+      />
       <br />
-      <button type="button" onClick={() => setText(exampleModel)}>
-        Use example file
-      </button>
+      Or <br />
+      <input
+        type="button"
+        onClick={() => setText(exampleModel)}
+        value="Use example file"
+      />
     </form>
   );
 }
