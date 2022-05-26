488270f141b309124aaab5b2c10520f0fb4ee9c2
geo-bert <s1067297@stud.sbg.ac.at>
2022-05-26 22:22:07 +0200

    Migrate form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,6 @@
+import PestControlIcon from "@mui/icons-material/PestControl";
+import UploadIcon from "@mui/icons-material/Upload";
+import { Avatar, Button, FormGroup, Typography } from "@mui/material";
 import { useState } from "react";
 import exampleModel from "../model/example-model";
 import { SetStringQ } from "../types/react-types";
@@ -5,28 +8,56 @@ import { SetStringQ } from "../types/react-types";
 function Form({ setText }: { setText: SetStringQ }) {
   let [file, setFile] = useState<File>();
   return (
-    <form className="file-form">
-      Select a valid BEATOR2 Model File <br />
-      <input
-        type="file"
-        accept=".btor2"
-        onChange={(e) => setFile(e.currentTarget.files!.item(0)!)}
-      />
-      <br />
-      <input
-        type="button"
+    <FormGroup
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        alignContent: "center",
+      }}>
+      <Avatar sx={{ mt: 5, bgcolor: "#26a69a" }}>
+        <PestControlIcon />
+      </Avatar>
+      <Typography component="h1" variant="h3" sx={{ mb: 2 }}>
+        Beatle
+      </Typography>
+      <Typography sx={{ mb: 2 }}>
+        Visualize and analyze BEATOR2 models
+      </Typography>
+      <Typography component="h2" variant="h6">
+        Please select a valid BEATOR2 file to start
+      </Typography>
+      <Button
+        sx={{ mt: 1, mb: 1, color: "#26a69a", borderColor: "#26a69a" }}
+        startIcon={<UploadIcon />}
+        variant="outlined"
+        component="label"
+        disableRipple>
+        Upload File
+        <input
+          type="file"
+          hidden
+          accept=".btor2"
+          onChange={(e) => setFile(e.currentTarget.files!.item(0)!)}
+        />
+      </Button>
+      <Button
+        sx={{ mt: 1, mb: 1, backgroundColor: "#26a69a" }}
         disabled={!file}
+        variant="contained"
         onClick={() => file!.text().then(setText)}
-        value="Evaluate Model"
-      />
-      <br />
-      Or <br />
-      <input
-        type="button"
+        disableRipple>
+        Evaluate Model
+      </Button>
+      <Button
+        sx={{ mt: 1, mb: 1, color: "#26a69a" }}
+        variant="text"
         onClick={() => setText(exampleModel)}
-        value="Use example file"
-      />
-    </form>
+        disableRipple>
+        Or Use Example File
+      </Button>
+    </FormGroup>
   );
 }
 
