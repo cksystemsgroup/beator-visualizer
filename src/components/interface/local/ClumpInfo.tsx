cb2c2c453683dffa1932914b36caee65d2077a80
geo-bert <s1067297@stud.sbg.ac.at>
2022-05-26 14:28:42 +0200

    Migrate ClumpInfo

--- src/components/interface/local/ClumpInfo.tsx
+++ src/components/interface/local/ClumpInfo.tsx
@@ -1,25 +1,20 @@
+import { Typography } from "@mui/material";
 import { ClumpNode } from "../../../types/node-types";
+import { Item, MyList } from "../../general/ListUtils";
 
 function ClumpInfo({ n }: { n: ClumpNode }) {
   return (
     <>
-      <p>
+      <Typography>
         This is a clump, a collection of one or more nodes of the same kind.
-      </p>
-      <ul>
-        <li>
-          <b>Type:</b> {n.nodeClass}
-        </li>
-        <li>
-          <b>Number of contained Nodes:</b> {n.size}
-        </li>
-        <li>
-          <b>Minimum depth:</b> {n.minDepth}
-        </li>
-        <li>
-          <b>Maximum depth:</b> {n.maxDepth}
-        </li>
-      </ul>
+      </Typography>{" "}
+      <br />
+      <MyList>
+        <Item property="Type" value={n.nodeClass} />
+        <Item property="Number of contained Nodes" value={n.size} />
+        <Item property="Minimum Depth" value={n.minDepth} />
+        <Item property="Maximum Depth" value={n.maxDepth} />
+      </MyList>
     </>
   );
 }
