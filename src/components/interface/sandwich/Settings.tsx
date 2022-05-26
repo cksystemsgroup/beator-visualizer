47de35190e4168fb405a9350c65990ac0a7b70f6
geo-bert <s1067297@stud.sbg.ac.at>
2022-05-26 15:33:37 +0200

    Migrate settings

--- src/components/interface/sandwich/Settings.tsx
+++ src/components/interface/sandwich/Settings.tsx
@@ -1,3 +1,4 @@
+import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 import { ClumpObject, SandwichProps } from "../../../types/react-types";
 
 function Settings({
@@ -9,146 +10,189 @@ function Settings({
   setShowPath,
 }: SandwichProps) {
   return (
-    <form>
-      <label>
-        <input
-          disabled={anyClumpActive(clump) || showPath}
-          checked={autoExpand}
-          type="checkbox"
-          onChange={() => {
-            setAutoExpand((x) => !x);
-            setTarget(undefined);
-          }}
-        />
-        Auto Expand
-      </label>
-      <details className="clumping-summary" open>
-        <summary>Clumping (disables collapsing)</summary>
-        <label>
-          <input
-            disabled={showPath}
-            type="checkbox"
-            checked={
-              clump.clumpIf &&
-              clump.clumpArith &&
-              clump.clumpConst &&
-              clump.clumpLogic &&
-              clump.clumpWrite &&
-              clump.clumpState &&
-              clump.clumpInput
-            }
+    <FormGroup>
+      <FormControlLabel
+        control={
+          <Checkbox
+            disableRipple
+            disabled={anyClumpActive(clump)}
+            checked={autoExpand}
             onChange={() => {
-              const b = !(
-                clump.clumpIf &&
-                clump.clumpArith &&
-                clump.clumpConst &&
-                clump.clumpLogic &&
-                clump.clumpWrite &&
-                clump.clumpState &&
-                clump.clumpInput
-              );
-              clump.setClumpIf(b);
-              clump.setClumpArith(b);
-              clump.setClumpConst(b);
-              clump.setClumpLogic(b);
-              clump.setClumpWrite(b);
-              clump.setClumpState(b);
-              clump.setClumpInput(b);
+              setAutoExpand((x) => !x);
+              setTarget(undefined);
             }}
           />
-          All
-        </label>
-        <br />
-        <label>
-          <input
+        }
+        label="Auto Expand"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            disableRipple
             disabled={showPath}
-            type="checkbox"
+            checked={Object.values(clump)
+              .filter((x) => typeof x == "boolean")
+              .reduce((a, x) => a && x, true)}
+            onChange={() => {
+              const b = !Object.values(clump).reduce((a, x) => a && x);
+              Object.values(clump)
+                .filter((x) => !(typeof x === "boolean"))
+                .forEach((x) => x(b));
+            }}
+          />
+        }
+        label="All"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            disableRipple
+            disabled={showPath}
             checked={clump.clumpIf}
             onChange={() => clump.setClumpIf((x) => !x)}
           />
-          If-then-else
-        </label>
-        <br />
-        <label>
-          <input
+        }
+        label="IfThenElse"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            disableRipple
             disabled={showPath}
-            type="checkbox"
             checked={clump.clumpLogic}
             onChange={() => clump.setClumpLogic((x) => !x)}
           />
-          Logic
-        </label>
-        <br />
-        <label>
-          <input
+        }
+        label="Logic"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            disableRipple
             disabled={showPath}
-            type="checkbox"
             checked={clump.clumpConst}
             onChange={() => clump.setClumpConst((x) => !x)}
           />
-          Constant
-        </label>
-        <br />
-        <label>
-          <input
+        }
+        label="Constant"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            disableRipple
             disabled={showPath}
-            type="checkbox"
             checked={clump.clumpState}
             onChange={() => clump.setClumpState((x) => !x)}
           />
-          State
-        </label>
-        <br />
-        <label>
-          <input
+        }
+        label="State"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            disableRipple
             disabled={showPath}
-            type="checkbox"
             checked={clump.clumpWrite}
             onChange={() => clump.setClumpWrite((x) => !x)}
           />
-          Write
-        </label>
-        <br />
-        <label>
-          <input
+        }
+        label="Write"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            disableRipple
             disabled={showPath}
-            type="checkbox"
             checked={clump.clumpArith}
             onChange={() => clump.setClumpArith((x) => !x)}
           />
-          Arithmetic
-        </label>{" "}
-        <br />
-        <label>
-          <input
+        }
+        label="Arithmetic"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            disableRipple
             disabled={showPath}
-            type="checkbox"
             checked={clump.clumpInput}
             onChange={() => clump.setClumpInput((x) => !x)}
           />
-          Input
-        </label>
-      </details>
-      <label>
-        <input
-          type="checkbox"
-          checked={showPath}
-          onChange={() => {
-            if (!showPath) {
-              clump.setClumpIf(false);
-              clump.setClumpArith(false);
-              clump.setClumpConst(false);
-              clump.setClumpLogic(false);
-              clump.setClumpWrite(false);
-              clump.setClumpState(false);
-              clump.setClumpInput(false);
-            }
-            setShowPath((x) => !x);
-          }}
-        />
-        Show Longest Path in Current Selection
-      </label>
-    </form>
+        }
+        label="Input"
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            disableRipple
+            checked={showPath}
+            onChange={() => {
+              if (!showPath) {
+                clump.setClumpIf(false);
+                clump.setClumpArith(false);
+                clump.setClumpConst(false);
+                clump.setClumpLogic(false);
+                clump.setClumpWrite(false);
+                clump.setClumpState(false);
+                clump.setClumpInput(false);
+              }
+              setShowPath((x) => !x);
+            }}
+          />
+        }
+        label="Show Longest Path in Current Selection"
+      />
+    </FormGroup>
+    //     <br />
+    //     <label>
+    //       <input
+    //         disabled={showPath}
+    //         type="checkbox"
+    //         checked={clump.clumpConst}
+    //         onChange={() => clump.setClumpConst((x) => !x)}
+    //       />
+    //       Constant
+    //     </label>
+    //     <br />
+    //     <label>
+    //       <input
+    //         disabled={showPath}
+    //         type="checkbox"
+    //         checked={clump.clumpState}
+    //         onChange={() => clump.setClumpState((x) => !x)}
+    //       />
+    //       State
+    //     </label>
+    //     <br />
+    //     <label>
+    //       <input
+    //         disabled={showPath}
+    //         type="checkbox"
+    //         checked={clump.clumpWrite}
+    //         onChange={() => clump.setClumpWrite((x) => !x)}
+    //       />
+    //       Write
+    //     </label>
+    //     <br />
+    //     <label>
+    //       <input
+    //         disabled={showPath}
+    //         type="checkbox"
+    //         checked={clump.clumpArith}
+    //         onChange={() => clump.setClumpArith((x) => !x)}
+    //       />
+    //       Arithmetic
+    //     </label>{" "}
+    //     <br />
+    //     <label>
+    //       <input
+    //         disabled={showPath}
+    //         type="checkbox"
+    //         checked={clump.clumpInput}
+    //         onChange={() => clump.setClumpInput((x) => !x)}
+    //       />
+    //       Input
+    //     </label>
+    //   </details>
   );
 }
 
